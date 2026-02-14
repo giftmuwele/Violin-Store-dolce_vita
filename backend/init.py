98551605f11feb2a108a42d087c604c0121c1685
Chipego <giftmuwele3@gmail.com>
2026-02-14 11:05:20 +0200

    Database for tracking users and products

---
+++ backend/init.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_cors import CORS
+from .config import Config
+from .extensions import mysql, bcrypt
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    CORS(app)
+
+    mysql.init_app(app)
+    bcrypt.init_app(app)
+
+    from .routes.auth import auth_bp
+    from .routes.products import product_bp
+    from .routes.cart import cart_bp
+    from .routes.orders import order_bp
+
+    app.register_blueprint(auth_bp, url_prefix="/api")
+    app.register_blueprint(product_bp, url_prefix="/api")
+    app.register_blueprint(cart_bp, url_prefix="/api")
+    app.register_blueprint(order_bp, url_prefix="/api")
+
+    return app
---
+++ backend/services.py
@@ -0,0 +1,46 @@
+from google.oauth2 import id_token
+from google.auth.transport import requests as grequests
+from flask import current_app
+from app.extensions import mysql
+import jwt, datetime
+
+def verify_google_token(token):
+    try:
+        return id_token.verify_oauth2_token(
+            token,
+            grequests.Request(),
+            current_app.config["GOOGLE_CLIENT_ID"]
+        )
+    except:
+        return None
+
+
+def find_or_create_user(idinfo):
+    email = idinfo["email"]
+    name = idinfo["name"]
+    google_id = idinfo["sub"]
+    picture = idinfo.get("picture")
+
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM users WHERE email=%s", (email,))
+    user = cur.fetchone()
+
+    if not user:
+        cur.execute("""
+            INSERT INTO users(name,email,google_id,profile_pic)
+            VALUES(%s,%s,%s,%s)
+        """, (name,email,google_id,picture))
+        mysql.connection.commit()
+
+        cur.execute("SELECT * FROM users WHERE email=%s", (email,))
+        user = cur.fetchone()
+
+    return user
+
+
+def generate_jwt(user):
+    return jwt.encode({
+        "user_id": user[0],
+        "role": user[5],
+        "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=2)
+    }, current_app.config["SECRET_KEY"], algorithm="HS256")
