c32f484d9e476729825267352bbc5000e557649d
Chipego <giftmuwele3@gmail.com>
2026-02-17 23:28:07 +0200

    push

--- backend/app/config.py
+++ backend/app/config.py
@@ -4,9 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY")
+    SECRET_KEY = os.getenv("SGOCSPX-C6XpcHs4CCoINf3zW8HczZ6WYpUVs")
     MYSQL_HOST = os.getenv("MYSQL_HOST")
     MYSQL_USER = os.getenv("MYSQL_USER")
     MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
     MYSQL_DB = os.getenv("MYSQL_DB")
-    GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
+    GOOGLE_CLIENT_ID = os.getenv("865720164989-ap4dpnm1vialmn0psm314aojmu9p2lms.apps.googleusercontent.com")
